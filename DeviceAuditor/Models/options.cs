fd7b1601d97cbfa17318985b532afa49ce36704f
Scott McIntosh <scottymac40love@gmail.com>
2026-06-06 09:45:15 -0700

    Update default vendor IDs in Options class

--- DeviceAuditor/Models/options.cs
+++ DeviceAuditor/Models/options.cs
@@ -6,7 +6,7 @@ namespace DeviceAuditor.Models
     public class Options
     {
         [Option('v', "vendors", Required = false, HelpText = "Comma-separated list of Vendor IDs to audit. Current defaults are 044F-Thrustmaster 4098-WINWING")]
-        public string Vendors { get; set; } = "044F,4098,346E,2F9A,047D,0951,33A1,0B05,04B4,231D";
+        public string Vendors { get; set; } = "044F,3F3A,4098,346E,2F9A,047D,0951,33A1,0B05,04B4,231D";
 
         [Option('a', "active-only", Required = false, HelpText = "Only report active controllers.")]
         public bool ActiveOnly { get; set; }
